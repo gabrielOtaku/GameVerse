ceab9336bda5a0018aa114bf7c78dc927975f500
Gabriel Herve <81375156+gabrielOtaku@users.noreply.github.com>
2025-11-03 13:27:19 -0500

    Refactor Accueil.php for styling and functionality
    Updated the Accueil.php file to include new Bootstrap and Tailwind CSS scripts, added preloader functionality, and enhanced the carousel section with additional images and improved structure.

--- TP-Final-Gabriel Tom Sevrin/Accueil.php
+++ TP-Final-Gabriel Tom Sevrin/Accueil.php
@@ -109,7 +109,10 @@
     </footer>
 
     <!--Bootstrap JS et Three.js-->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.min.js"
+        integrity="sha384-G/EV+4j2dNv+tEPo3++6LCgdCROaejBqfUeNjuKAiuXbjrxilcCdDz6ZAVfHWe1Y" crossorigin="anonymous"></script>
     <script src="JS/monJS.js"></script>
 
     <!--Script pour l'effet 3D des icônes-->
